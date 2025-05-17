5388c644958dbdbe4527b24fa54b531429dd35eb
Denis Craig <admin@deniscraig.com>
2025-05-17 11:15:47 +0100

    Fix typo for rust vscode extension

--- packages/vscodium-rust.nix
+++ packages/vscodium-rust.nix
@@ -6,7 +6,7 @@ pkgs.vscode-with-extensions.override {
     pkgs.open-vsx.github.vscode-github-actions
     pkgs.open-vsx.jnoortheen.nix-ide
     pkgs.open-vsx.mkhl.direnv
-    pkgs.open-vsx.rust-lang.rust-analyser
+    pkgs.open-vsx.rust-lang.rust-analyzer
     pkgs.open-vsx.tamasfe.even-better-toml
     pkgs.open-vsx.vadimcn.vscode-lldb
   ];
